23ed0adf8209e93689b0e86d26b4074ad0919035
rucarpe <rucarpe@icloud.com>
2023-04-02 13:13:23 +0200

    Sube

--- php-poo/clases.php
+++ php-poo/clases.php
@@ -4,8 +4,8 @@
 # Definir una clase y sus métodos
 # 
 */
-class Coche {
 
+class Coche {
     // Propiedades de la clase Coche
     public $color = "Rojo";
     public $marca = "Audi";
@@ -14,19 +14,56 @@ class Coche {
     public $hp = 105;
     public $puertas = 5;
 
-    // Métodos (Funciones) de la clase Coche
+    // DEFINIR (CREAR) MÉTODOS (Funciones) de la clase Coche
     public function getColor() {
         // Acceder a la propiedad indicada $this
         return $this->color;
     }
     // Cambiar valores a los métodos
     // Es necesario pasarle el parámetro
+            // ## set no se suele usar en la difinición de una clase
     public function setColor($color) {
         // Le cambiamos el color a la popiedad de la clase Coche
         $this->color = "Negro";
     }
 
-    public function setModelo($modelo) {
-        $this->modelo = "A4";
+    // Función public function atributo (parametro-variable){}
+    public function getModelo() {
+        return $this->modelo = "A4";
     }
-}
+
+    // MÉTODO PARA ACELERAR EL COCHE
+    public function acelerar() {
+        $this->velocidad_max++;
+    }
+    // MÉTODO PARA FRENAR EL COCHE
+    public function frenar() {
+        $this->velocidad_max--;
+    }
+
+    public function getVelocidad_max() {
+        return $this->velocidad_max;
+    }
+
+} // Fin clase Coche
+
+// Crear objeto / Instanciar la clase
+$coche = new Coche();
+$coche2 = new Coche();
+
+// Acceder a los métodos de la clase de forma separada de cada objeto.
+echo $coche->getVelocidad_max();
+echo "<br>";
+// 
+$coche ->acelerar();
+$coche ->acelerar();
+$coche ->acelerar();
+$coche ->acelerar();
+$coche ->acelerar();
+echo "<br>";
+echo $coche->getVelocidad_max();
+echo "<br>";
+var_dump($coche);
+echo "<br>";
+var_dump($coche2);
+?>
